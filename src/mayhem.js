4341d7035c76d062f820b83beadbd6b806a8dabf
runtimeTerror418 <ccosmos416@gmail.com>
2023-06-10 01:41:45 -0700

    Leaving JS source code behind

---
+++ src/mayhem.js
@@ -0,0 +1,11 @@
+export { Renderer } from "./core/renderer.js";
+export { Clock } from "./core/clock.js";
+export { OrthographicCamera } from "./core/camera.js";
+export { imageLoader } from "./core/loader.js";
+export { Vector2, vec2 } from "./maths/vector2.js";
+export { Vector3, vec3 } from "./maths/vector3.js";
+export { Mat4 } from "./maths/mat4.js";
+export { createVBO, updateVBO, createEBO, updateEBO } from "./buffer/arrayBuffer.js";
+export { createShader, setShader, setUniforms, setAttributes } from "./shader/shader.js";
+export { Texture } from "./shader/texture.js";
+//# sourceMappingURL=mayhem.js.map
